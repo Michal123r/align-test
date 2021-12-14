88c76631820c9e6f2d7257fba2cfe51682be59bb
michal <michal12r@gmail.com>
2021-12-14 21:22:53 +0200

    image entity

---
+++ src/app/model/image.ts
@@ -0,0 +1,27 @@
+
+export interface Iimage {
+    id: number;
+    author: string;
+    width: number;
+    height: number;
+    url: string;
+    download_url: string;
+  }
+  
+  export class Image {
+    id: number;
+    author: string;
+    width: number;
+    height: number;
+    url: string;
+    download_url: string;
+  
+    constructor(id: number, author: string, width: number, height: number, url: string, download_url: string) {
+      this.id = id;
+      this.author = author;
+      this.width = width;
+      this.height = height;
+      this.url = url;
+      this.download_url = download_url;
+    }
+  }
