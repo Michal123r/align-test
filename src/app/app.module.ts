7e723a4e1c0c09bbb9ef950add0a68671b3eda89
michal <michal12r@gmail.com>
2022-04-27 21:07:42 +0300

    board npm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxChessBoardModule } from 'ngx-chess-board';
 
 import { AppComponent } from './app.component';
+import { BoardComponent } from './components/board/board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxChessBoardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
